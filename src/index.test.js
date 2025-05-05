254c2ce66ea29922af3597fe0666aadb09e5d9bc
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-05-05 15:01:28 +0900

    Add calculator.divide

--- src/index.test.js
+++ src/index.test.js
@@ -6,16 +6,20 @@ test('Capitalize chambers to Chambers', () => {
 
 test('Reverse chambers to srebmahc', () => {
     expect(reverseString('chambers')).toBe('srebmahc');
-})
+});
 
 test('Add 24 and 13 to be 37', () => {
     expect(calculator.add(24, 13)).toBe(37);
-})
+});
 
 test('Subtract 13 from 24 to be 11', () => {
     expect(calculator.subtract(24, 13)).toBe(11);
-})
+});
 
 test('Multiply 24 and 13 to be 312', () => {
     expect(calculator.multiply(24, 13)).toBe(312);
-})
+});
+
+test('Divide 6 from 24 to be 4', () => {
+    expect(calculator.divide(24, 6)).toBe(4);
+});
