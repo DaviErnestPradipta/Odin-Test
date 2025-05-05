adfc7f5f95f8cae89c25cd61436399bcc64b9290
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-05-05 14:48:40 +0900

    Add reverseString function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 export function capitalize(string) {
+    if (string === '') return string;
     return string[0].toUpperCase() + string.slice(1);
+}
+
+export function reverseString(string) {
+    if (string === '') return string;
+    return string[string.length - 1] + reverseString(string.slice(0, string.length -1));
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,9 @@
-import {capitalize} from './index.js';
+import {capitalize, reverseString} from './index.js';
 
 test('Capitalize chambers to Chambers', () => {
     expect(capitalize('chambers')).toBe('Chambers');
 });
+
+test('Reverse chambers to srebmahc', () => {
+    expect(reverseString('chambers')).toBe('srebmahc');
+})
