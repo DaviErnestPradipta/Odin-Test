74c9e093382fff4e6d1c61650c6fcd99efa7cb72
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-05-05 14:57:16 +0900

    Add calculator.subtract

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ export function reverseString(string) {
 }
 
 export const calculator = {
-    add: (a, b) => a + b
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
 }
--- src/index.test.js
+++ src/index.test.js
@@ -11,3 +11,7 @@ test('Reverse chambers to srebmahc', () => {
 test('Add 24 and 13 to be 37', () => {
     expect(calculator.add(24, 13)).toBe(37);
 })
+
+test('Subtract 13 from 24 to be 11', () => {
+    expect(calculator.subtract(24, 13)).toBe(11);
+})
